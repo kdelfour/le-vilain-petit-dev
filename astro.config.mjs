38c4453d3a130033454cbc23bc6a28ccf6f65937
Kevin Delfour <kevin.delfour@maxds.fr>
2024-11-20 10:26:57 +0100

    :sparkles: Add search page

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,10 +10,10 @@ import pagefind from "astro-pagefind";
 // https://astro.build/config
 export default defineConfig({
   site: "https://kdelfour.github.io",
-  base: "/blog",
+  base: "/blog/",
   output: "static",
-  // build: {
-  //   format: "file",
-  // },
+  build: {
+    format: "file",
+  },
   integrations: [mdx(), sitemap(), tailwind(), icon(), pagefind()],
 });
