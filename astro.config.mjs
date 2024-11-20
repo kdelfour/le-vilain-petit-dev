4ab76e00f32936afe266b93fc3e302fb0fc4ca9a
Kevin Delfour <kevin.delfour@maxds.fr>
2024-11-20 09:26:56 +0100

    :sparkles: Add search page

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,11 +5,15 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
+import pagefind from "astro-pagefind";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://kdelfour.github.io",
   base: "/blog",
   output: "static",
-  integrations: [mdx(), sitemap(), tailwind(), icon()],
+  // build: {
+  //   format: "file",
+  // },
+  integrations: [mdx(), sitemap(), tailwind(), icon(), pagefind()],
 });
